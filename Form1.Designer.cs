a392e79d56dd7873de166bf3db8cbb9c939e3c31
henri225 <64895717+henri225@users.noreply.github.com>
2021-04-21 14:15:28 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿
+namespace Gerestion_Ecole
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.btnDeconnection = new System.Windows.Forms.Button();
+            this.btnCours = new System.Windows.Forms.Button();
+            this.btnProfesseur = new System.Windows.Forms.Button();
+            this.btnEtudiant = new System.Windows.Forms.Button();
+            this.btnInscription = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panelTitre = new System.Windows.Forms.Panel();
+            this.labelTitre = new System.Windows.Forms.Label();
+            this.panelMenu.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panelTitre.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
+            this.panelMenu.Controls.Add(this.btnDeconnection);
+            this.panelMenu.Controls.Add(this.btnCours);
+            this.panelMenu.Controls.Add(this.btnProfesseur);
+            this.panelMenu.Controls.Add(this.btnEtudiant);
+            this.panelMenu.Controls.Add(this.btnInscription);
+            this.panelMenu.Controls.Add(this.panel1);
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(194, 450);
+            this.panelMenu.TabIndex = 0;
+            // 
+            // btnDeconnection
+            // 
+            this.btnDeconnection.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnDeconnection.FlatAppearance.BorderSize = 0;
+            this.btnDeconnection.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDeconnection.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDeconnection.ForeColor = System.Drawing.Color.White;
+            this.btnDeconnection.Location = new System.Drawing.Point(0, 259);
+            this.btnDeconnection.Name = "btnDeconnection";
+            this.btnDeconnection.Size = new System.Drawing.Size(194, 46);
+            this.btnDeconnection.TabIndex = 6;
+            this.btnDeconnection.Text = "Déconnection";
+            this.btnDeconnection.UseVisualStyleBackColor = true;
+            // 
+            // btnCours
+            // 
+            this.btnCours.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnCours.FlatAppearance.BorderSize = 0;
+            this.btnCours.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCours.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCours.ForeColor = System.Drawing.Color.White;
+            this.btnCours.Location = new System.Drawing.Point(0, 213);
+            this.btnCours.Name = "btnCours";
+            this.btnCours.Size = new System.Drawing.Size(194, 46);
+            this.btnCours.TabIndex = 5;
+            this.btnCours.Text = "Cours";
+            this.btnCours.UseVisualStyleBackColor = true;
+            // 
+            // btnProfesseur
+            // 
+            this.btnProfesseur.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnProfesseur.FlatAppearance.BorderSize = 0;
+            this.btnProfesseur.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnProfesseur.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProfesseur.ForeColor = System.Drawing.Color.White;
+            this.btnProfesseur.Location = new System.Drawing.Point(0, 167);
+            this.btnProfesseur.Name = "btnProfesseur";
+            this.btnProfesseur.Size = new System.Drawing.Size(194, 46);
+            this.btnProfesseur.TabIndex = 4;
+            this.btnProfesseur.Text = "Professeur";
+            this.btnProfesseur.UseVisualStyleBackColor = true;
+            // 
+            // btnEtudiant
+            // 
+            this.btnEtudiant.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnEtudiant.FlatAppearance.BorderSize = 0;
+            this.btnEtudiant.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEtudiant.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEtudiant.ForeColor = System.Drawing.Color.White;
+            this.btnEtudiant.Location = new System.Drawing.Point(0, 121);
+            this.btnEtudiant.Name = "btnEtudiant";
+            this.btnEtudiant.Size = new System.Drawing.Size(194, 46);
+            this.btnEtudiant.TabIndex = 3;
+            this.btnEtudiant.Text = "Etudiant";
+            this.btnEtudiant.UseVisualStyleBackColor = true;
+            // 
+            // btnInscription
+            // 
+            this.btnInscription.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnInscription.FlatAppearance.BorderSize = 0;
+            this.btnInscription.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnInscription.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInscription.ForeColor = System.Drawing.Color.White;
+            this.btnInscription.Location = new System.Drawing.Point(0, 75);
+            this.btnInscription.Name = "btnInscription";
+            this.btnInscription.Size = new System.Drawing.Size(194, 46);
+            this.btnInscription.TabIndex = 2;
+            this.btnInscription.Text = "Inscription";
+            this.btnInscription.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(76)))));
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(194, 75);
+            this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(200, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(188, 75);
+            this.panel2.TabIndex = 1;
+            // 
+            // panelTitre
+            // 
+            this.panelTitre.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(150)))), ((int)(((byte)(136)))));
+            this.panelTitre.Controls.Add(this.labelTitre);
+            this.panelTitre.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTitre.Location = new System.Drawing.Point(194, 0);
+            this.panelTitre.Name = "panelTitre";
+            this.panelTitre.Size = new System.Drawing.Size(606, 75);
+            this.panelTitre.TabIndex = 1;
+            // 
+            // labelTitre
+            // 
+            this.labelTitre.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.labelTitre.AutoSize = true;
+            this.labelTitre.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitre.ForeColor = System.Drawing.Color.White;
+            this.labelTitre.Location = new System.Drawing.Point(253, 27);
+            this.labelTitre.Name = "labelTitre";
+            this.labelTitre.Size = new System.Drawing.Size(72, 25);
+            this.labelTitre.TabIndex = 0;
+            this.labelTitre.Text = "HOME";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panelTitre);
+            this.Controls.Add(this.panelMenu);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panelMenu.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panelTitre.ResumeLayout(false);
+            this.panelTitre.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelMenu;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnDeconnection;
+        private System.Windows.Forms.Button btnCours;
+        private System.Windows.Forms.Button btnProfesseur;
+        private System.Windows.Forms.Button btnEtudiant;
+        private System.Windows.Forms.Button btnInscription;
+        private System.Windows.Forms.Panel panelTitre;
+        private System.Windows.Forms.Label labelTitre;
+    }
+}
+
